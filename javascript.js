42eae0642dc2e8afd7c7759181f7ac66cdf3e414
Hayden Westfall <hwestfall04@gmail.com>
2024-12-27 12:39:50 -0800

    Added logic for computer and human choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3); 
+    return computerChoice;
+    //0 = rock, 1 = paper, 2 = scissors
+}
+
+function getHumanChoice() {
+    let = humanChoice = prompt("Rock, Paper, Scissors?");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
+
