0336ffb21a125a298e87ec97d00975e426d3af4b
Hayden Westfall <hwestfall04@gmail.com>
2024-12-27 13:05:09 -0800

    function playRound completed - else if statements

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-
+const humanScore = 0;
+const computerScore = 0;
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3); 
     return computerChoice;
@@ -11,3 +12,32 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice == 0 && userChoice == "paper"){
+        console.log("Computer chose rock, you won this round.");
+        humanScore++;
+    }
+    else if (computerChoice == 0 && userChoice =="scissors"){
+        console.log("Computer chose rock, you lose this round.");
+        computerScore++;
+    }
+    else if (computerChoice == 1 && userChoice =="rock"){
+        console.log("Computer chose paper, you lose this round.");
+        computerScore++;
+    }
+    else if (computerChoice == 1 && userChoice =="scissors"){
+        console.log("Computer chose paper, you win this round.");
+        humanScore++;
+    }
+    else if (computerChoice == 2 && userChoice =="rock"){
+        console.log("Computer chose scissors, you win this round.");
+        humanScore++;
+    }
+    else if (computerChoice == 2 && userChoice =="paper"){
+        console.log("Computer chose scissors, you lose this round.");
+        computerScore++;
+    }
+    else {
+        console.log("A tie.")
+    }
+}
